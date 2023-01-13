0790c34114138df5a571fd5fc507fa9219a7522b
Cuckss <akshatchaturvedi60@gmail.com>
2023-01-13 16:31:55 +0530

    Add Bank Application

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name password and balance to create account");
+        String name=sc.next();
+        String password=sc.next();
+        double balance=sc.nextDouble();
+        SBIUser user=new SBIUser(name,balance,password);
+        HDFCUser user1=new HDFCUser (name,balance,password);
+        //adding money
+        System.out.println(user.addMoney(100000));
+        //withdrawing money
+        System.out.println("enter the amount you want to withdraw:");
+      int money=sc.nextInt();
+        System.out.println("enter your password:");
+        String enteredPassword=sc.next();
+        System.out.println(user.withdrawMoney(money,enteredPassword));
+        //calculate rateOfInterest;
+        System.out.println(user.calculateInterest(10));
+    }
+}
---
+++ src/SBIUser.java
@@ -0,0 +1,92 @@
+import java.util.Objects;
+import java.util.UUID;
+
+public class SBIUser implements BankInterface {
+        private String name;
+        private String accountNo;
+        private double balance;
+        private String password;
+        private static double rateOfInterest;
+
+        public SBIUser(String name, double balance, String password) {
+            //provided by the user;
+            this.name = name;
+            this.balance = balance;
+            this.password = password;
+            //we are analysing;
+            this.rateOfInterest=6.5;
+            this.accountNo=String.valueOf(UUID.randomUUID());
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAccountNo() {
+            return accountNo;
+        }
+
+        public void setAccountNo(String accountNo) {
+            this.accountNo = accountNo;
+        }
+
+        public double getBalance() {
+            return balance;
+        }
+
+        public void setBalance(double balance) {
+            this.balance = balance;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public static double getRateOfInterest() {
+            return rateOfInterest;
+        }
+
+        public static void setRateOfInterest(double rateOfInterest) {
+            SBIUser.rateOfInterest = rateOfInterest;
+        }
+
+        @Override
+        public double checkBalance() {
+            return this.balance;
+        }
+
+        @Override
+        public String addMoney(int amount) {
+            balance=balance+amount;
+            return"beti paise aa gaye dekh:"+balance;
+        }
+
+        @Override
+        public String withdrawMoney(int amount, String enteredPassword) {
+            if(Objects.equals(enteredPassword,password)){
+                if(amount>balance){
+                    return "Sorry bhai paise nahi hai itne";
+                }else{
+                    balance=balance-amount;
+                    return"ha beti paise lele";
+                }
+            } else{
+                return"Wrong password";
+            }
+        }
+
+        @Override
+        public double calculateInterest(int years) {
+            return(balance*years*rateOfInterest)/100;
+        }
+    }
+
+
